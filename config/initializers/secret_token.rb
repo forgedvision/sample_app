6a987e79a8702e66c641cc7b20065cd60bee5906
Forged Vision <forgedvision@gmail.com>
2014-06-15 18:43:13 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = '7d80a0899b51099d4e2cc2fd8979ffe680ce5f1ea20af017ff6f7b9fea4749d65aba1e711798656d7838786f7137ac76c2d1d16bb2c7044a2a2d97d976ff017e'
