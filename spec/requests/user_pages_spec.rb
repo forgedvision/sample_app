a260867e32588561c8755dcc64f061ee65bb2516
Forged Vision <forgedvision@gmail.com>
2014-06-30 19:29:29 -0400

    finishing up with exercises

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -16,9 +16,20 @@ describe "User Pages" do
     let(:submit) { "Create my account" }
 
     describe "with invalid information" do
+
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
+
+      describe "after submit" do
+        before { click_button submit }
+
+        it { should have_title("Sign up") }
+        it { should have_content("errors") }
+        it { should have_content("can't be blank") }
+        it { should have_content("is too short") }
+      end
+
     end
 
     describe "with valid information" do
@@ -32,6 +43,16 @@ describe "User Pages" do
       it "should create a user" do
           expect { click_button submit }.to change(User, :count).by(1)
       end
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+
+      end
+
     end
   end
 
